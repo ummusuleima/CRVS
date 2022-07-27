22fb85b843a30dd8ee21a1fbaf10447fbabe515f
Brian Temu <bryangasper2124@gmail.com>
2022-07-27 12:02:53 +0300

    refractoring

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,25 @@
 module.exports = {
   content: ["./src/**/*.{html,js}"],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        crvsblue: {
+          700: "#0369a1",
+          800: "#075985",
+          900: "#0c4a6e",
+        },
+        crvsgreen: {
+          300: "#86efac",
+          700: "#15803d",
+          800: "#166534",
+          900: "#14532d",
+        },
+        crvsicon: {
+          100: "#e2e8f0",
+          900: "#0f172a",
+        },
+      },
+    },
     fontFamily: {
       sans: ["DM Sans", "sans-serif"],
     },
