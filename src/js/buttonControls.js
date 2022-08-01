a95fc6b14e7c4683abd28a9b8b9d2a0f03b10b3a
Brian Temu <bryangasper2124@gmail.com>
2022-08-01 10:47:54 +0300

    report

--- src/js/buttonControls.js
+++ src/js/buttonControls.js
@@ -2,18 +2,18 @@ $(document).ready(function () {
   $("#open_sidebar").click(() => {
     $("#sidebar").removeClass("hidden");
     $("#sidebar").addClass("block");
-    console.log("hellow");
+    
   });
   
   $("#close_sidebar").click(() => {
     $("#sidebar").addClass("hidden");
     $("#sidebar").removeClass("block");
-    console.log("hellow");
+    
   });
 
   $("#logout_button").focus(() => {
     $("#logout_menu").show()
-    console.log("hellow");
+    
   });
   $("#logout_button").focusout(function () {
     $("#logout_menu").hide();
