f973d3983340162474f3672b891b181228d271aa
Brian Temu <bryangasper2124@gmail.com>
2022-08-02 16:13:42 +0300

    menu

--- src/js/buttonControls.js
+++ src/js/buttonControls.js
@@ -2,19 +2,22 @@ $(document).ready(function () {
   $("#open_sidebar").click(() => {
     $("#sidebar").removeClass("hidden");
     $("#sidebar").addClass("block");
-    
   });
-  
+
   $("#close_sidebar").click(() => {
     $("#sidebar").addClass("hidden");
     $("#sidebar").removeClass("block");
-    
   });
 
-  $("#logout_button").click(() => {
-    $("#logout_menu").toggle() 
-  });
-  // $("#logout_button").focusout(function () {
-  //   $("#logout_menu").hide();
+  // $("#logout_button").focus(() => {
+  //   $("#logout_menu").show()
   // });
+
+  // $("#logout_button").focusout(function () {
+  //   $("#logout_menu").hide()
+  // });
+
+  $("#logout_button").click(() => {
+    $("#logout_menu").toggle("slow")
+  });
 });
