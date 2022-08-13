ccd15dd86c6b23f431fd05842afff2ece7912b1b
Brian Temu <bryangasper2124@gmail.com>
2022-08-13 11:18:25 +0300

    next design

--- src/js/buttonControls.js
+++ src/js/buttonControls.js
@@ -14,10 +14,13 @@ $(document).ready(function () {
     $("#childInfoId").removeClass("stepper-active");
     $("#motherInfoId").addClass("stepper-active ");
 
-    $(".heightstepper").attr("style", "height: 2010px");
+    $(".heightstepper").attr("style", "height: 1800px");
 
-    $("#motherDiv").addClass(" slide-in-right animation fast");
+    // $("#motherDiv").addClass(" slide-in-right animation fast");
+
+    // $("#childDiv").addClass(" slide-out-left animation fast");
     $("#motherDiv").attr("style", "display: block");
+
     $("#childDiv").attr("style", "display: none");
     $("#childInfoId").addClass("stepper-completed");
   });
@@ -25,14 +28,16 @@ $(document).ready(function () {
   $("#prevChild").click(() => {
     document.getElementById("mainpage").scrollIntoView();
     $("#childInfoId").addClass("stepper-active");
-    $("#motherInfoId").removeClass("stepper-active ");
+    $("#motherInfoId").removeClass("stepper-active");
 
-    $(".heightstepper").attr("style", "height: 800px");
+    $(".heightstepper").attr("style", "height: 1000px");
 
-    $("#motherDiv").addClass(" slide-out-left animation fast");
+    // $("#childDiv").addClass(" slide-in-left animation fast");
+
+    // $("#motherDiv").addClass(" slide-out-right animation fast");
+
     $("#childDiv").attr("style", "display: block");
     $("#motherDiv").attr("style", "display: none");
-    // $("#childInfoId").addClass("stepper-completed");
   });
 
   $("#fatherNext").click(() => {
@@ -40,11 +45,54 @@ $(document).ready(function () {
     $("#motherInfoId").removeClass("stepper-active");
     $("#fatherInfoId").addClass("stepper-active ");
 
-    $(".heightstepper").attr("style", "height: 1800px");
+    $(".heightstepper").attr("style", "height: 1300px");
 
-    $("#fatherDiv").addClass(" slide-in-right animation fast");
+    // $("#fatherDiv").addClass(" slide-in-right animation fast");
     $("#fatherDiv").attr("style", "display: block");
     $("#motherDiv").attr("style", "display: none");
     $("#fatherInfoId").addClass("stepper-completed");
+  });
+
+  $("#prevMother").click(() => {
+    document.getElementById("mainpage").scrollIntoView();
+    $("#motherInfoId").addClass("stepper-active");
+    $("#fatherInfoId").removeClass("stepper-active");
+
+    $(".heightstepper").attr("style", "height: 1800px");
+
+    // $("#childDiv").addClass(" slide-in-left animation fast");
+
+    // $("#motherDiv").addClass(" slide-out-right animation fast");
+
+    $("#motherDiv").attr("style", "display: block");
+    $("#fatherDiv").attr("style", "display: none");
+  });
+
+  $("#InfoNext").click(() => {
+    document.getElementById("mainpage").scrollIntoView();
+    $("#fatherInfoId").removeClass("stepper-active");
+    $("#InfoId").addClass("stepper-active ");
+
+    $(".heightstepper").attr("style", "height:1000px");
+
+    // $("#fatherDiv").addClass(" slide-in-right animation fast");
+    $("#infomDiv").attr("style", "display: block");
+    $("#fatherDiv").attr("style", "display: none");
+    // $("#fatherInfoId").addClass("stepper-completed");
+  });
+
+  $("#prevFather").click(() => {
+    document.getElementById("mainpage").scrollIntoView();
+    $("#fatherInfoId").addClass("stepper-active");
+    $("#InfoId").removeClass("stepper-active");
+
+    $(".heightstepper").attr("style", "height: 1300px");
+
+    // $("#childDiv").addClass(" slide-in-left animation fast");
+
+    // $("#motherDiv").addClass(" slide-out-right animation fast");
+
+    $("#fatherDiv").attr("style", "display: block");
+    $("#infomDiv").attr("style", "display: none");
   });
 });
