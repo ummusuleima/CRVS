52b4ad4570f3abe15a122bdafdfc7f7abf326918
Brian Temu <bryangasper2124@gmail.com>
2022-08-15 15:36:25 +0300

    form and cards

--- src/js/buttonControls.js
+++ src/js/buttonControls.js
@@ -14,7 +14,7 @@ $(document).ready(function () {
     $("#childInfoId").removeClass("stepper-active");
     $("#motherInfoId").addClass("stepper-active ");
 
-    $(".heightstepper").attr("style", "height: 1800px");
+    $(".heightstepper").attr("style", "height: 2010px");
 
     // $("#motherDiv").addClass(" slide-in-right animation fast");
 
@@ -58,7 +58,7 @@ $(document).ready(function () {
     $("#motherInfoId").addClass("stepper-active");
     $("#fatherInfoId").removeClass("stepper-active");
 
-    $(".heightstepper").attr("style", "height: 1800px");
+    $(".heightstepper").attr("style", "height: 2010px");
 
     // $("#childDiv").addClass(" slide-in-left animation fast");
 
@@ -95,4 +95,95 @@ $(document).ready(function () {
     $("#fatherDiv").attr("style", "display: block");
     $("#infomDiv").attr("style", "display: none");
   });
+
+   $("#attNext").click(() => {
+     document.getElementById("mainpage").scrollIntoView();
+     $("#InfoId").removeClass("stepper-active");
+     $("#attId").addClass("stepper-active ");
+
+     $(".heightstepper").attr("style", "height:3000px");
+
+     // $("#fatherDiv").addClass(" slide-in-right animation fast");
+     $("#attDiv").attr("style", "display: block");
+     $("#infomDiv").attr("style", "display: none");
+     $("#InfoId").addClass("stepper-completed");
+   });
+
+  $("#decNext").click(() => {
+    document.getElementById("mainpage").scrollIntoView();
+    $("#attId").removeClass("stepper-active");
+    $("#decId").addClass("stepper-active ");
+
+    $(".heightstepper").attr("style", "height:3000px");
+
+    // $("#fatherDiv").addClass(" slide-in-right animation fast");
+    $("#decDiv").attr("style", "display: block");
+    $("#attDiv").attr("style", "display: none");
+    $("#attId").addClass("stepper-completed");
+  });
+
+  $("#prevInfro").click(() => {
+    document.getElementById("mainpage").scrollIntoView();
+    $("#InfoId").addClass("stepper-active");
+    $("#attId").removeClass("stepper-active");
+
+    $(".heightstepper").attr("style", "height: 1300px");
+
+    // $("#childDiv").addClass(" slide-in-left animation fast");
+
+    // $("#motherDiv").addClass(" slide-out-right animation fast");
+
+    $("#infomDiv").attr("style", "display: block");
+    $("#attDiv").attr("style", "display: none");
+  });
+
+  $("#prevAtt").click(() => {
+    document.getElementById("mainpage").scrollIntoView();
+    $("#attId").addClass("stepper-active");
+    $("#decId").removeClass("stepper-active");
+
+    $(".heightstepper").attr("style", "height: 1300px");
+
+    // $("#childDiv").addClass(" slide-in-left animation fast");
+
+    // $("#motherDiv").addClass(" slide-out-right animation fast");
+
+    $("#attDiv").attr("style", "display: block");
+    $("#decDiv").attr("style", "display: none");
+  });
+
+
+
+  const input = document.getElementById("fileInput");
+  // const avatar = document.getElementById("avatar");
+  // const textArea = document.getElementById("textAreaExample");
+
+  const convertBase64 = (file) => {
+    return new Promise((resolve, reject) => {
+      const fileReader = new FileReader();
+      fileReader.readAsDataURL(file);
+
+      fileReader.onload = () => {
+        resolve(fileReader.result);
+      };
+
+      fileReader.onerror = (error) => {
+        reject(error);
+      };
+    });
+  };
+
+  const uploadImage = async (event) => {
+    const file = event.target.files[0];
+    const base64 = await convertBase64(file);
+
+    $("#fileview").attr("src", base64);
+    console.log(base64)
+  };
+
+  input.addEventListener("change", (e) => {
+    uploadImage(e);
+  });
+
+
 });
